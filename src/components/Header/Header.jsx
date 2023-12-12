3d81739cac84f39b152f264926835b37daf9ce44
Jonathan Silva <jonathan.silva167@gmail.com>
2023-12-12 15:04:33 -0500

    add basic contact modal

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,8 +4,10 @@ import { Link as ScrollLink } from 'react-scroll';
 import "./Header.scss";
 
 import resumePdf from '../../assets/Jonathan-Silva-Software-Engineer-Resume-2023.pdf';
+
 const Header = () => {
     const [showResume, setShowResume] = useState(false);
+    const [showContact, setShowContact] = useState(false);
 
     const handleshowResume = (e) => {
         e.preventDefault();
@@ -16,6 +18,15 @@ const Header = () => {
         setShowResume(false);
     };
 
+    const handleContact = (e) => {
+        e.preventDefault();
+        setShowContact(true)
+    }
+
+    const handleCloseContact = (e) => {
+        setShowContact(false);
+    };
+
     return (
         <>
             <header className="header">
@@ -24,15 +35,28 @@ const Header = () => {
                         <li className="header-nav__items"><Link to="/">Home</Link></li>
                         <li className="header-nav__items"><ScrollLink to="projects" smooth={true} duration={1000} offset={-40}>Projects</ScrollLink></li>
                         <li className="header-nav__items"><Link to="/resume" onClick={handleshowResume}>Resume</Link></li>
-                        <li className="header-nav__items"><Link to="/contact">Contact</Link></li>
+                        <li className="header-nav__items"><Link to="/contact" onClick={handleContact}>Contact</Link></li>
                     </ul>
                 </nav>
             </header>
             {showResume && (
-                <div className="resume-modal">
-                    <button className="resume-modal__button" onClick={handleCloseResume}>X to close</button>
-                    <iframe className="resume-modal__pdf" src={`${resumePdf}#zoom=75`} title="resume"></iframe>
-                    <a href={resumePdf} download>Download Resume</a>
+                <div className="modal">
+                    <button className="modal__button" onClick={handleCloseResume}>Close</button>
+                    <iframe className="modal__pdf" src={`${resumePdf}#zoom=55`} title="resume"></iframe>
+                    <a href={resumePdf} className="modal__button" download>Download Resume</a>
+                </div>
+            )}
+            {showContact && (
+                <div className="modal">
+                    <button className="modal__button" onClick={handleCloseContact}>Close</button>
+                    <section className="modal__contact">
+                        <h2 className="modal__contact--header">Email</h2>
+                        <a className="modal__contact--anchor" href="mailto:jonathan.silva167@gmail.com" >
+                            jonathan.silva167@gmail.com
+                        </a>
+                        <h2 className="modal__contact--header">Phone</h2>
+                        <a href="tel:+16313274452" className="modal__contact--anchor">+1 (631) 327-4452</a>
+                    </section>
                 </div>
             )}
         </>
