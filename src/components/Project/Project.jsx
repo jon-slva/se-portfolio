68c5aef3f9372273ead9018ed7222278167cd909
Jonathan Silva <jonathan.silva167@gmail.com>
2023-12-12 18:53:25 -0500

    Fix conditional rendering of GitHub links in Project component

--- src/components/Project/Project.jsx
+++ src/components/Project/Project.jsx
@@ -8,13 +8,13 @@ const Project = ({ title, description, imgSrc, github1, github2 }) => {
             <h3 className="project__title">{title}</h3>
             <img src={imgSrc} alt={title} className="project__img" />
             <div className='project__links'>
-                {github1 ? <a href={github1.link} className="project__link">
+                {github1.link ? <a href={github1.link} className="project__link">
                     <img className="project__link--github-logo" src={logoGithub} alt="" />
                     <p>
                         {github1.label}
                     </p>
                 </a> : null}
-                {github2 ? <a href={github2.link} className="project__link">
+                {github2.link ? <a href={github2.link} className="project__link">
                     <img className="project__link--github-logo" src={logoGithub} alt="" />
                     <p>
                         {github2.label}
