70d2d3251ea0696cda364df957e54600d1c14d78
Jonathan Silva <jonathan.silva167@gmail.com>
2024-02-22 15:00:19 -0500

    Update resume PDF path in Header component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,68 +3,67 @@ import { Link } from "react-router-dom";
 import { Link as ScrollLink } from 'react-scroll';
 import "./Header.scss";
 
-import resumePdf from '../../assets/Jonathan-Silva-Software-Engineer-Resume-2023.pdf';
+import resumePdf from '../../assets/resumes/Jonathan-Silva-Software-Engineer-Resume-2024.pdf';
 
 const Header = () => {
-    // const [showResume, setShowResume] = useState(false);
-    const [showContact, setShowContact] = useState(false);
-    const [isClosing, setIsClosing] = useState(false);
+	const [showContact, setShowContact] = useState(false);
+	const [isClosing, setIsClosing] = useState(false);
 
-    const mobileBreakpoint = 768; // Mobile breakpoint
-    const tabletBreakpoint = 1280; // Tablet breakpoint
-    let zoomLevel = undefined;
+	const mobileBreakpoint = 768; // Mobile breakpoint
+	const tabletBreakpoint = 1280; // Tablet breakpoint
+	let zoomLevel = undefined;
 
-    if (window.innerWidth <= mobileBreakpoint) {
-        zoomLevel = 40;
-    } else if (window.innerWidth <= tabletBreakpoint) {
-        zoomLevel = 60;
-    } else {
-        zoomLevel = 80;
-    }
-
-
-    const handleContact = (e) => {
-        e.preventDefault();
-        setIsClosing(false);
-        setShowContact(true)
-    }
-
-    const handleCloseContact = (e) => {
-        e.preventDefault();
-        setIsClosing(true);
-        setTimeout(() => {
-            setIsClosing(false);
-            setShowContact(false);
-        }, 500);
-    };
+	if (window.innerWidth <= mobileBreakpoint) {
+		zoomLevel = 40;
+	} else if (window.innerWidth <= tabletBreakpoint) {
+		zoomLevel = 60;
+	} else {
+		zoomLevel = 80;
+	}
 
 
-    return (
-        <>
-            <header className="header">
-                <nav className="header-nav">
-                    <ul>
-                        <li className="header-nav__items"><ScrollLink to="projects" smooth={true} duration={1000} offset={-40}>Projects</ScrollLink></li>
-                        <li className="header-nav__items"><a href={resumePdf} download>Resume</a></li>
-                        <li className="header-nav__items"><a onClick={handleContact}>Contact</a></li>
-                    </ul>
-                </nav>
-            </header>
-            {showContact && (
-                <div className={`modal ${isClosing ? 'closing' : ''}`}>
-                    <button className="modal__button" onClick={handleCloseContact}>Close</button>
-                    <section className="modal__contact">
-                        <h2 className="modal__contact--header">Email</h2>
-                        <a className="modal__contact--anchor" href="mailto:jonathan.silva167@gmail.com" >
-                            jonathan.silva167@gmail.com
-                        </a>
-                        <h2 className="modal__contact--header">Phone</h2>
-                        <a href="tel:+16313274452" className="modal__contact--anchor">+1 (631) 327-4452</a>
-                    </section>
-                </div>
-            )}
-        </>
-    )
+	const handleContact = (e) => {
+		e.preventDefault();
+		setIsClosing(false);
+		setShowContact(true)
+	}
+
+	const handleCloseContact = (e) => {
+		e.preventDefault();
+		setIsClosing(true);
+		setTimeout(() => {
+			setIsClosing(false);
+			setShowContact(false);
+		}, 500);
+	};
+
+
+	return (
+		<>
+			<header className="header">
+				<nav className="header-nav">
+					<ul>
+						<li className="header-nav__items"><ScrollLink to="projects" smooth={true} duration={1000} offset={-40}>Projects</ScrollLink></li>
+						<li className="header-nav__items"><a href={resumePdf} download>Resume</a></li>
+						<li className="header-nav__items"><a onClick={handleContact}>Contact</a></li>
+					</ul>
+				</nav>
+			</header>
+			{showContact && (
+				<div className={`modal ${isClosing ? 'closing' : ''}`}>
+					<button className="modal__button" onClick={handleCloseContact}>Close</button>
+					<section className="modal__contact">
+						<h2 className="modal__contact--header">Email</h2>
+						<a className="modal__contact--anchor" href="mailto:jonathan.silva167@gmail.com" >
+							jonathan.silva167@gmail.com
+						</a>
+						<h2 className="modal__contact--header">Phone</h2>
+						<a href="tel:+16313274452" className="modal__contact--anchor">+1 (631) 327-4452</a>
+					</section>
+				</div>
+			)}
+		</>
+	)
 }
 
 export default Header
--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -8,106 +8,106 @@ import './Projects.scss';
 import Project from '../../components/Project/Project';
 
 const Projects = () => {
-    return (
-        <aside className="projects">
-            <h2 className="projects__header">Projects</h2>
+	return (
+		<aside className="projects">
+			<h2 className="projects__header">Projects</h2>
 
-            <div className="projects-container">
-                <Project
-                    title={
-                        <>
-                            AWS Hackathon <img src={winnerMedal} alt="1st Place Winner" className="projects-container__svg-image" />
-                        </>
-                    }
-                    description={
-                        <ul className='projects__description'>
-                            <li>
-                                Redesign of AWS Data Exchange Marketplace interface.
-                                4 teams assigned to the task.
-                            </li>
-                            <li>
-                                Your team: 2 Software Engineers, 2 UX Designers, 1 Data Scientist.
-                            </li>
-                            <li>
-                                Implemented AI-powered Suggested Datasets section.
-                            </li>
-                            <li>
-                                AI-powered search was decisive in winning the competition.
-                            </li>
-                        </ul>
-                    }
-                    imgSrc={AWSPic}
-                    github1={{ link: 'https://github.com/malyesh/aws-hackathon', label: 'GitHub Repo' }}
-                    github2={{ link: '', label: '' }}
-                />
+			<div className="projects-container">
+				<Project
+					title={
+						<>
+							AWS Hackathon <img src={winnerMedal} alt="1st Place Winner" className="projects-container__svg-image" />
+						</>
+					}
+					description={
+						<ul className='projects__description'>
+							<li>
+								Redesign of AWS Data Exchange Marketplace interface.
+								4 teams assigned to the task.
+							</li>
+							<li>
+								Your team: 2 Software Engineers, 2 UX Designers, 1 Data Scientist.
+							</li>
+							<li>
+								Implemented AI-powered Suggested Datasets section.
+							</li>
+							<li>
+								AI-powered search was decisive in winning the competition.
+							</li>
+						</ul>
+					}
+					imgSrc={AWSPic}
+					github1={{ link: 'https://github.com/malyesh/aws-hackathon', label: 'GitHub Repo' }}
+					github2={{ link: '', label: '' }}
+				/>
 
-                <Project
-                    title="Mentor Me"
-                    description={
-                        <ul className='projects__description'>
-                            <li>
-                                My bootcamp capstone project.
-                            </li>
-                            <li>
-                                Your team: 2 Software Engineers, 2 UX Designers, 1 Data Scientist.
-                            </li>
-                            <li>
-                                Platform created to connect learners with experienced mentors.
-                            </li>
-                            <li>
-                                Utilizes Three.js for rendering interactions and user locations on a 3D globe.
-                            </li>
-                        </ul>
-                    }
-                    imgSrc={mentorPic}
-                    github1={{ link: 'https://github.com/jon-slva/mentor-me-client', label: 'Client' }}
-                    github2={{ link: 'https://github.com/jon-slva/mentor-me-server', label: 'Server' }}
-                />
-            </div>
+				<Project
+					title="Mentor Me"
+					description={
+						<ul className='projects__description'>
+							<li>
+								My bootcamp capstone project.
+							</li>
+							<li>
+								Your team: 2 Software Engineers, 2 UX Designers, 1 Data Scientist.
+							</li>
+							<li>
+								Platform created to connect learners with experienced mentors.
+							</li>
+							<li>
+								Utilizes Three.js for rendering interactions and user locations on a 3D globe.
+							</li>
+						</ul>
+					}
+					imgSrc={mentorPic}
+					github1={{ link: 'https://github.com/jon-slva/mentor-me-client', label: 'Client' }}
+					github2={{ link: 'https://github.com/jon-slva/mentor-me-server', label: 'Server' }}
+				/>
+			</div>
 
-            <div className="projects-container">
-                <Project
-                    title="Flixnet Movie Picker"
-                    description={
-                        <ul className='projects__description'>
-                            <li>
-                                First 24 hour hackathon project.
-                            </li>
-                            <li>
-                                Worked with two software engineers.
-                            </li>
-                            <li>
-                                Utilized a third-party API for ideation and implementation.
-                            </li>
-                        </ul>
-                    }
-                    imgSrc={flixPic}
-                    github1={{ link: 'https://github.com/jon-slva/flixnet', label: 'GitHub Repo' }}
-                    github2={{ link: '', label: '' }}
-                />
+			<div className="projects-container">
+				<Project
+					title="Flixnet Movie Picker"
+					description={
+						<ul className='projects__description'>
+							<li>
+								First 24 hour hackathon project.
+							</li>
+							<li>
+								Worked with two software engineers.
+							</li>
+							<li>
+								Utilized a third-party API for ideation and implementation.
+							</li>
+						</ul>
+					}
+					imgSrc={flixPic}
+					github1={{ link: 'https://github.com/jon-slva/flixnet', label: 'GitHub Repo' }}
+					github2={{ link: '', label: '' }}
+				/>
 
-                <Project
-                    title="This or That"
-                    description={
-                        <ul className='projects__description'>
-                            <li>
-                                Second 24 hour hackathon project.
-                            </li>
-                            <li>
-                                Developed a platform for fun and controversial topic debates.
-                            </li>
-                            <li>
-                                Participants debate for or against declaration statements.
-                            </li>
-                        </ul>
-                    }
-                    imgSrc={thisPic}
-                    github1={{ link: 'https://github.com/GabrielCorderoDeveloper/this-or-that', label: 'Client' }}
-                    github2={{ link: 'https://github.com/jon-slva/this-or-that-server', label: 'Server' }}
-                />
-            </div>
-        </aside>
-    )
+				<Project
+					title="This or That"
+					description={
+						<ul className='projects__description'>
+							<li>
+								Second 24 hour hackathon project.
+							</li>
+							<li>
+								Developed a platform for fun and controversial topic debates.
+							</li>
+							<li>
+								Participants debate for or against declaration statements.
+							</li>
+						</ul>
+					}
+					imgSrc={thisPic}
+					github1={{ link: 'https://github.com/GabrielCorderoDeveloper/this-or-that', label: 'Client' }}
+					github2={{ link: 'https://github.com/jon-slva/this-or-that-server', label: 'Server' }}
+				/>
+			</div>
+		</aside>
+	)
 }
 
 export default Projects
