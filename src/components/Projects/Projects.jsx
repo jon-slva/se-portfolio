375176376ca95c99f304f7a89d45645cd8ac836a
Jonathan Silva <jonathan.silva167@gmail.com>
2024-02-27 23:26:18 -0500

    Add live link and hover effects to project images, revise bullets, add stack and future features to projects

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -8,106 +8,131 @@ import './Projects.scss';
 import Project from '../../components/Project/Project';
 
 const Projects = () => {
-    return (
-        <aside className="projects">
-            <h2 className="projects__header">Projects</h2>
+	return (
+		<aside className="projects">
+			<h2 className="projects__header">Projects</h2>
 
-            <div className="projects-container">
-                <Project
-                    title={
-                        <>
-                            AWS Hackathon <img src={winnerMedal} alt="1st Place Winner" className="projects-container__svg-image" />
-                        </>
-                    }
-                    description={
-                        <ul className='projects__description'>
-                            <li>
-                                Redesign of AWS Data Exchange Marketplace interface.
-                                4 teams assigned to the task.
-                            </li>
-                            <li>
-                                Your team: 2 Software Engineers, 2 UX Designers, 1 Data Scientist.
-                            </li>
-                            <li>
-                                Implemented AI-powered Suggested Datasets section.
-                            </li>
-                            <li>
-                                AI-powered search was decisive in winning the competition.
-                            </li>
-                        </ul>
-                    }
-                    imgSrc={AWSPic}
-                    github1={{ link: 'https://github.com/malyesh/aws-hackathon', label: 'GitHub Repo' }}
-                    github2={{ link: '', label: '' }}
-                />
+			<div className="projects-container">
+				<Project
+					title={
+						<>
+							AWS Hackathon <img src={winnerMedal} alt="1st Place Winner" className="projects-container__svg-image" />
+						</>
+					}
+					description={
+						<ul className='projects__description'>
+							<li>
+								24 hours to redesign the AWS Data Exchange Marketplace interface.
+								4 teams assigned to the task.
+							</li>
+							<li>
+								Worked in a team w/ 2 Software Engineers, 2 UX Designers, & 1 Data Scientist.
+							</li>
+							<li>
+								Implemented AI-powered "Suggested Datasets" search results.
+							</li>
+							<li>
+								AI-powered search was decisive in winning the competition.
+							</li>
+							<p className='projects__stack'>
+								<strong>
+									React, Axios, SASS, REST API, Open AI GPT-4
+								</strong>
+							</p>
+						</ul>
+					}
+					imgSrc={AWSPic}
+					github1={{ link: 'https://github.com/malyesh/aws-hackathon', label: 'GitHub Repo' }}
+					github2={{ link: '', label: '' }}
+					liveLink={'https://awshackathon.johnny5ive.com/'}
+				/>
 
-                <Project
-                    title="Mentor Me"
-                    description={
-                        <ul className='projects__description'>
-                            <li>
-                                My bootcamp capstone project.
-                            </li>
-                            <li>
-                                Your team: 2 Software Engineers, 2 UX Designers, 1 Data Scientist.
-                            </li>
-                            <li>
-                                Platform created to connect learners with experienced mentors.
-                            </li>
-                            <li>
-                                Utilizes Three.js for rendering interactions and user locations on a 3D globe.
-                            </li>
-                        </ul>
-                    }
-                    imgSrc={mentorPic}
-                    github1={{ link: 'https://github.com/jon-slva/mentor-me-client', label: 'Client' }}
-                    github2={{ link: 'https://github.com/jon-slva/mentor-me-server', label: 'Server' }}
-                />
-            </div>
+				<Project
+					title="Mentor Me"
+					description={
+						<ul className='projects__description'>
+							<li>
+								A fullstack web-application for connecting learners with experienced mentors in a variety of topics. The mission is to reduce the barrier to entry for learning new things, and eliminate the arduous initial research needed to begin a new hobby or career.
+							</li>
+							<li>
+								Utilizes Three.js for rendering interactions and user locations on a 3D globe.
+							</li>
+							<li>
+								GeoApify API converts user addresses to latitude & longitude for rendering on the globe.
+							</li>
+							<li>
+								Distict dashboard interface for mentors and mentees.
+							</li>
+							<p className='projects__stack'>
+								<strong>Future features:</strong> Video conferencing API, Chat API, Stripe Payment Gateway, Calendar scheduling for video meetings, cirriculum builder with cirriculum history, grading, React Native port, group mentorships.
+							</p>
+							<p className='projects__stack'>
+								<strong>
+									React, Node, MySQL, Three.js, Express, Axios, SASS, REST API, JWT User Authentication
+								</strong>
+							</p>
+						</ul>
+					}
+					imgSrc={mentorPic}
+					github1={{ link: 'https://github.com/jon-slva/mentor-me-client', label: 'Client' }}
+					github2={{ link: 'https://github.com/jon-slva/mentor-me-server', label: 'Server' }}
+				/>
+			</div>
 
-            <div className="projects-container">
-                <Project
-                    title="Flixnet Movie Picker"
-                    description={
-                        <ul className='projects__description'>
-                            <li>
-                                First 24 hour hackathon project.
-                            </li>
-                            <li>
-                                Worked with two software engineers.
-                            </li>
-                            <li>
-                                Utilized a third-party API for ideation and implementation.
-                            </li>
-                        </ul>
-                    }
-                    imgSrc={flixPic}
-                    github1={{ link: 'https://github.com/jon-slva/flixnet', label: 'GitHub Repo' }}
-                    github2={{ link: '', label: '' }}
-                />
+			<div className="projects-container">
+				<Project
+					title="Flixnet Movie Picker"
+					description={
+						<ul className='projects__description'>
+							<li>
+								Helps users find movies to watch based on their mood & how much time they have. Death to endless watch queues!
+							</li>
+							<li>
+								Utilizes a third-party API for movie posters and data.
+							</li>
+							<p className='projects__stack'>
+								<strong>Future features:</strong> Shortlist movies, random pick, streaming platform availability for each film, TV Shows, voting with friends.
+							</p>
+							<p className='projects__stack'>
+								<strong>
+									React, Axios, SASS, REST API
+								</strong>
+							</p>
+						</ul>
+					}
+					imgSrc={flixPic}
+					github1={{ link: 'https://github.com/jon-slva/flixnet', label: 'GitHub Repo' }}
+					github2={{ link: '', label: '' }}
+					liveLink={'https://flixnet.johnny5ive.com/'}
+				/>
 
-                <Project
-                    title="This or That"
-                    description={
-                        <ul className='projects__description'>
-                            <li>
-                                Second 24 hour hackathon project.
-                            </li>
-                            <li>
-                                Developed a platform for fun and controversial topic debates.
-                            </li>
-                            <li>
-                                Participants debate for or against declaration statements.
-                            </li>
-                        </ul>
-                    }
-                    imgSrc={thisPic}
-                    github1={{ link: 'https://github.com/GabrielCorderoDeveloper/this-or-that', label: 'Client' }}
-                    github2={{ link: 'https://github.com/jon-slva/this-or-that-server', label: 'Server' }}
-                />
-            </div>
-        </aside>
-    )
+				<Project
+					title="This or That"
+					description={
+						<ul className='projects__description'>
+							<li>
+								A simple fullstack CRUD web-app where users can debate fun and/or controversial topics.
+							</li>
+							<li>
+								Utilizes a built-from-scratch RESTful API for data storage.
+							</li>
+							<li>
+								This project was a team effort with 2 fellow developers.
+							</li>
+							<p className='projects__stack'>
+								<strong>
+									React, Node, Express, Axios, SASS, REST API
+								</strong>
+							</p>
+						</ul>
+					}
+					imgSrc={thisPic}
+					github1={{ link: 'https://github.com/GabrielCorderoDeveloper/this-or-that', label: 'Client' }}
+					github2={{ link: 'https://github.com/jon-slva/this-or-that-server', label: 'Server' }}
+				/>
+			</div>
+		</aside>
+	)
 }
 
 export default Projects
